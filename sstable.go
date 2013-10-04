8e1048e781a59ae8a25e56033cc43228ef54f5e5
Duane Bailey <bailey.d.r@gmail.com>
2013-10-04 14:48:58 -0400

    Added decode.

--- sstable.go
+++ sstable.go
@@ -1,5 +1,9 @@
 package sstable
 
+import (
+  "fmt"
+)
+
 type MemTable map[string][]byte
 
 const maxVarintBytes = 6
@@ -16,6 +20,24 @@ func EncodeVarint(x uint64) []byte {
   return buf[0:n]
 }
 
+func DecodeVarint(buf []byte) (x uint64, n int) {
+  // x, n already 0
+  for shift := uint(0); shift < 64; shift += 7 {
+    if n >= len(buf) {
+      return 0, 0
+    }
+    b := uint64(buf[n])
+    n++
+    x |= (b & 0x7F) << shift
+    if (b & 0x80) == 0 {
+      return x, n
+    }
+  }
+
+  // The number is too large to represent in a 64-bit value.
+  return 0, 0
+}
+
 func EncodePair(key string, value []byte) ([]byte, error) {
   key_length_bytes := EncodeVarint(uint64(len(key)))
   value_length_bytes := EncodeVarint(uint64(len(value)))
@@ -23,6 +45,31 @@ func EncodePair(key string, value []byte) ([]byte, error) {
   encoded_value := append(value_length_bytes, value...)
   encoded_pair := append(encoded_key, encoded_value...)
   return encoded_pair, nil
+}
+
+func DecodeBuf(buf []byte) (decoded []byte, err error) {
+  length, read := DecodeVarint(buf)
+  if read == 0 {
+    err = fmt.Errorf("Read 0 bytes")
+    return
+  }
+  decoded = buf[read:length]
+  return
+}
+
+func DecodePair(buf []byte) error {
+  key, err := DecodeBuf(buf)
+  if err != nil {
+    return err
+  }
+
+  value, err := DecodeBuf(buf[len(key):])
+  if err != nil {
+    return err
+  }
+
+  fmt.Printf("Key: %v, value: %v", key, value)
+  return nil
 }
 
 func Flush(table *MemTable, filename string) error {
